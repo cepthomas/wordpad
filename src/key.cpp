f3591c105269b0f4979b7eede778fdb34682743b
cepthomas <cepthomas@gmail.com>
2026-02-19 12:17:37 -0600

    Optionally disable registry access.

--- src/key.cpp
+++ src/key.cpp
@@ -18,46 +18,81 @@
 static char BASED_CODE THIS_FILE[] = __FILE__;
 #endif
 
+// Log reg writes without actually performing.
+#define _FAKE
+#include "Windows.h"
+#define MSIZE 500
+char message[MSIZE+1];
+
+
 /////////////////////////////////////////////////////////////////////////////
 // CKey
 
 void CKey::Close()
 {
+#ifdef _FAKE
+	snprintf(message, MSIZE, "Close() [%ld]\n", (long)m_hKey);
+	OutputDebugStringA(message);
+#else
 	if (m_hKey != NULL)
 	{
 		LONG lRes = RegCloseKey(m_hKey);
 		ASSERT(lRes == ERROR_SUCCESS);
 		m_hKey = NULL;
 	}
+#endif
 }
 
 BOOL CKey::Create(HKEY hKey, LPCTSTR lpszKeyName)
 {
+#ifdef _FAKE
+	snprintf(message, MSIZE, "Create() [%ld] [%ws]\n", (long)hKey, lpszKeyName);
+	m_hKey = hKey;
+	OutputDebugStringA(message);
+	return TRUE;
+#else
 	ASSERT(hKey != NULL);
 	return (RegCreateKey(hKey, lpszKeyName, &m_hKey) == ERROR_SUCCESS);
+#endif
 }
 
 BOOL CKey::Open(HKEY hKey, LPCTSTR lpszKeyName)
 {
+#ifdef _FAKE
+	snprintf(message, MSIZE, "Open() [%ld] [%ws]\n", (long)hKey, lpszKeyName);
+	OutputDebugStringA(message);
+	return TRUE;
+#else
 	ASSERT(hKey != NULL);
 	return (RegOpenKey(hKey, lpszKeyName, &m_hKey) == ERROR_SUCCESS);
+#endif
 }
 
 BOOL CKey::SetStringValue(LPCTSTR lpszValue, LPCTSTR lpszValueName)
 {
+#ifdef _FAKE
+	snprintf(message, MSIZE, "SetStringValue() [%ld] [%ws] [%ws]\n", (long)m_hKey, lpszValue, lpszValueName);
+	OutputDebugStringA(message);
+	return TRUE;
+#else
 	ASSERT(m_hKey != NULL);
-	return (RegSetValueEx(m_hKey, lpszValueName, NULL, REG_SZ,
-		(BYTE * const)lpszValue, (lstrlen(lpszValue)+1)*sizeof(TCHAR)) == ERROR_SUCCESS);
+	return (RegSetValueEx(m_hKey, lpszValueName, NULL, REG_SZ, (BYTE* const)lpszValue, (lstrlen(lpszValue) + 1) * sizeof(TCHAR)) == ERROR_SUCCESS);
+#endif
 }
 
 BOOL CKey::GetStringValue(CString& str, LPCTSTR lpszValueName)
 {
+#ifdef _FAKE
+	snprintf(message, MSIZE, "GetStringValue() [%ld] [%ws]\n", (long)m_hKey, lpszValueName);
+	OutputDebugStringA(message);
+	str = "something";
+	return TRUE;
+#else
 	ASSERT(m_hKey != NULL);
 	str.Empty();
 	DWORD dw = 0;
 	DWORD dwType = 0;
-	LONG lRes = RegQueryValueEx(m_hKey, (LPTSTR)lpszValueName, NULL, &dwType,
-		NULL, &dw);
+	LONG lRes = RegQueryValueEx(m_hKey, (LPTSTR)lpszValueName, NULL, &dwType, NULL, &dw);
 	if (lRes == ERROR_SUCCESS)
 	{
 		ASSERT(dwType == REG_SZ);
@@ -68,4 +103,5 @@ BOOL CKey::GetStringValue(CString& str, LPCTSTR lpszValueName)
 		return TRUE;
 	}
 	return FALSE;
+#endif
 }
